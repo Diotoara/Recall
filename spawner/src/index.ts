4090a8a2a05ff85c92afd9d90079ded3f4f0c2f3
Diotoara <aapka.5ryan@gmail.com>
2025-08-29 18:23:01 +0530

    selenium

---
+++ spawner/src/index.ts
@@ -0,0 +1,11 @@
+import { Builder, Browser, By, Key, until } from 'selenium-webdriver';
+
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.CHROME).build()
+  try {
+    await driver.get('http://meet.google.com/hev-hruj-pkd')
+    await driver.wait(until.titleIs('anime sake - Google Search'), 10000)
+  } finally {
+    await driver.quit()
+  }
+})()
