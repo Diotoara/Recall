18d33cebf152495ad4cb3d50f399f7c6a37216fa
Diotoara <aapka.5ryan@gmail.com>
2025-08-30 13:29:21 +0530

    joining-mic-issu_resolve

--- spawner/src/index.ts
+++ spawner/src/index.ts
@@ -1,7 +1,11 @@
 import { Builder, Browser, By, Key, until } from 'selenium-webdriver';
+import { Options} from 'selenium-webdriver/chrome';
 
 (async function example() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build()
+  const options = new Options({})
+  options.addArguments("--disable-blinkfeatures=AutomationControlled")
+  options.addArguments("--use-fake-ui-for-media-stream");
+  let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
   try {
     await driver.get('http://meet.google.com/hev-hruj-pkd')
     await driver.wait(until.titleIs('anime sake - Google Search'), 10000)
